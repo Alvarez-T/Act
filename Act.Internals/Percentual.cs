afdce0bc9a4930bf631895c410cdd6bd9df58c3f
Alvarez-T <thiago.alvarez2012@gmail.com>
2025-02-03 00:53:07 -0300

    Created Percentual Value Object Type.

---
+++ Act.Internals/Percentual.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+
+namespace Act.Utils;
+
+public readonly record struct Percentual
+{
+    private readonly decimal _percentageValue;
+    public int DecimalPlaces { get; }
+    public static int DefaultDecimalPlaces { get; set; } = 2;
+
+    public Percentual(decimal percentageValue, int decimalPlaces)
+    {
+        DecimalPlaces = decimalPlaces;
+        _percentageValue = Math.Round(percentageValue, decimalPlaces, MidpointRounding.AwayFromZero);
+    }
+
+    public static implicit operator Percentual(decimal value) => new(value, DefaultDecimalPlaces);
+    public static implicit operator decimal(Percentual percent) => percent._percentageValue / 100m;
+
+    public static Percentual Parse(string s, int? decimalPlaces = null)
+    {
+        if (s is null) throw new ArgumentNullException(nameof(s));
+
+        var span = s.AsSpan().Trim();
+        bool hasPercentage = !span.IsEmpty && span[^1] == '%';
+        var numberSpan = hasPercentage ? span[..^1] : span;
+
+        if (!decimal.TryParse(numberSpan, NumberStyles.Number, CultureInfo.InvariantCulture, out var value))
+            throw new FormatException("Invalid number format");
+
+        decimal percentageValue = hasPercentage ? value : value * 100m;
+        int places = decimalPlaces ?? DefaultDecimalPlaces;
+
+        return new Percentual(percentageValue, places);
+    }
+
+    public decimal ApplyTo(decimal value) => value * _percentageValue / 100m;
+
+    public override string ToString() =>
+        _percentageValue.ToString($"F{DecimalPlaces}", CultureInfo.InvariantCulture);
+
+    public string ToStringWithSymbol() =>
+        ToString() + " %";
+}
---
+++ Act.Internals/PercentualExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace Act.Utils;
+
+public static class PercentualExtensions
+{
+    public static Percentual AsPercentual(this decimal percentageValue, int decimalPlaces) =>
+        new Percentual(percentageValue, decimalPlaces);
+
+    public static Percentual ToPercentual(this decimal fractionValue, int decimalPlaces) =>
+        new Percentual(fractionValue * 100m, decimalPlaces);
+
+    public static Percentual ToPercentual(this string s, int? decimalPlaces = null) =>
+        Percentual.Parse(s, decimalPlaces);
+
+    public static decimal CalculatePercentage(this Percentual percent, decimal value) =>
+        percent.ApplyTo(value);
+
+    public static Percentual PercentageDifferenceFrom(this decimal newValue, decimal originalValue,
+        int? decimalPlaces = null)
+    {
+        if (originalValue == 0)
+            throw new DivideByZeroException("Original value cannot be zero when calculating percentage difference");
+
+        decimal difference = newValue - originalValue;
+        decimal percentageValue = (difference / originalValue) * 100m;
+        int places = decimalPlaces ?? Percentual.DefaultDecimalPlaces;
+
+        return percentageValue.AsPercentual(places);
+    }
+}
